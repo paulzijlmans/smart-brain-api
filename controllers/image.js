f2041a240f21d22459c374e9ef4e65b9654d322e
Paul Zijlmans <paulzijlmans@hotmail.com>
2021-08-09 09:09:42 +0200

    completed clarifai in backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,12 +1,17 @@
-import Clarifai from 'clarifai';
+const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
   apiKey: 'f66a5232e20740b2b15f43b94af615c3'
 });
 
-const handleApiCall = (req, res) => {
+const handleApiCall = () => (req, res) => {
+  const { input } = req.body;
   app.models
-    .predict('f76196b43bbd45c99b4f3cd8e8b40a8a', req.body.input);
+    .predict('f76196b43bbd45c99b4f3cd8e8b40a8a', input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(() => res.status(400).json('Unable to work with API'));
 }
 
 const handleImage = (db) => (req, res) => {
