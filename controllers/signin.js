4d85881493bc1e629752f2f597fa3de2b722592e
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-02-01 20:04:50 +0100

    Store JWT

--- controllers/signin.js
+++ controllers/signin.js
@@ -32,10 +32,18 @@ const signToken = (email) => {
   return jwt.sign(jwtPayload, process.env.JWT_SECRET, { expiresIn: '2 days' });
 }
 
+const setToken = (key, value) => {
+  return Promise.resolve(redisClient.set(key, value));
+}
+
 const createSession = (user) => {
   const { email, id } = user;
   const token = signToken(email);
-  return { success: 'true', userId: id, token };
+  return setToken(token, id)
+    .then(() => {
+      return { success: 'true', userId: id, token }
+    })
+    .catch(console.log);
 }
 
 const signinAuthentication = (db, bcrypt) => (req, res) => {
