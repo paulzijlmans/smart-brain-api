103d1a7e15e1dd7cd2510b3f4f24111adb827442
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-02-01 12:29:22 +0100

    User Authentication

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,25 +1,36 @@
-const handleSignin = (db, bcrypt) => (req, res) => {
+const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).json('Incorrect form submission');
+    return Promise.reject('Incorrect form submission');
   }
-  db.select('email', 'hash').from('login')
+  return db.select('email', 'hash').from('login')
     .where('email', email)
     .then(data => {
       const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
-        db.select('*').from('users').where('email', email)
-          .then(user => {
-            res.json(user[0]);
-          })
-          .catch(() => res.status(400).json('Unable to get user'));
+        return db.select('*').from('users').where('email', email)
+          .then(user => user[0])
+          .catch(() => Promise.reject('Unable to get user'));
       } else {
-        res.status(400).json('Wrong credentials');
+        Promise.reject('Wrong credentials');
       }
     })
-    .catch(() => res.status(400).json("Wrong credentials"));
+    .catch(() => Promise.reject("Wrong credentials"));
+}
+
+const getAuthTokenId = () => {
+  console.log('auth ok');
+}
+
+const signinAuthentication = (db, bcrypt) => (req, res) => {
+  const { authorization } = req.headers;
+  return authorization
+    ? getAuthTokenId()
+    : handleSignin(db, bcrypt, req, res)
+      .then(data => res.json(data))
+      .catch(err => res.status(400).json(err));
 }
 
 module.exports = {
-  handleSignin
+  signinAuthentication: signinAuthentication
 }
--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => { res.send('It is working!') });
-app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/signin', signin.signinAuthentication(db, bcrypt));
 app.post('/register', register.handleRegister(db, bcrypt));
 app.get('/profile/:id', profile.handleProfileGet(db));
 app.post('/profile/:id', profile.handleProfileUpdate(db));
