f8570afe42aeb8a6bc048bd4f07147a194e35221
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-02-01 12:45:16 +0100

    Send JWT

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
@@ -22,12 +24,28 @@ const getAuthTokenId = () => {
   console.log('auth ok');
 }
 
+const signToken = (email) => {
+  const jwtPayload = { email };
+  return jwt.sign(jwtPayload, process.env.JWT_SECRET, { expiresIn: '2 days' });
+}
+
+const createSession = (user) => {
+  const { email, id } = user;
+  const token = signToken(email);
+  return { success: 'true', userId: id, token};
+}
+
 const signinAuthentication = (db, bcrypt) => (req, res) => {
   const { authorization } = req.headers;
   return authorization
     ? getAuthTokenId()
     : handleSignin(db, bcrypt, req, res)
-      .then(data => res.json(data))
+      .then(data => {
+        return data.id && data.email
+          ? createSession(data)
+          : Promise.reject(data);
+      })
+      .then(session => res.json(session))
       .catch(err => res.status(400).json(err));
 }
 
