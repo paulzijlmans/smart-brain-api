c1a67ac9340ec6d70127eb852895d2322580ef1b
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-02-08 11:55:50 +0100

    Add authorization

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const auth = require('./controllers/authorization');
 
 const db = knex({
   client: 'pg',
@@ -27,9 +28,9 @@ app.use(cors());
 app.get('/', (req, res) => { res.send('It is working!') });
 app.post('/signin', signin.signinAuthentication(db, bcrypt));
 app.post('/register', register.handleRegister(db, bcrypt));
-app.get('/profile/:id', profile.handleProfileGet(db));
-app.post('/profile/:id', profile.handleProfileUpdate(db));
-app.put('/image', image.handleImage(db));
-app.post('/imageUrl', image.handleApiCall());
+app.get('/profile/:id', auth.requireAuth, profile.handleProfileGet(db));
+app.post('/profile/:id', auth.requireAuth, profile.handleProfileUpdate(db));
+app.put('/image', auth.requireAuth, image.handleImage(db));
+app.post('/imageUrl', auth.requireAuth, image.handleApiCall());
 
 app.listen(process.env.PORT || 3000, () => console.log(`App is running on port ${process.env.PORT || 3000}!`));
